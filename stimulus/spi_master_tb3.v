a9197b66ba3390085d6ffdb68203692c32dc0f04
Sara Sorensen Grandone <sagr4610@colorado.edu>
2016-02-01 17:46:54 -0700

    Modified spi_master_tb3.v

--- stimulus/spi_master_tb3.v
+++ stimulus/spi_master_tb3.v
@@ -30,9 +30,11 @@
 module spi_master_tb3;
 
 parameter SYSCLK_PERIOD = 38.46154;// 26MHZ
+parameter ORBIT_TIME = 500; // 
 
 reg SYSCLK;
 reg NSYSRESET;
+reg [7:0] DATAIN;
 
 initial
 begin
@@ -49,9 +51,8 @@ begin
         NSYSRESET = 1'b1;
     #(SYSCLK_PERIOD * 4 )
         NSYSRESET = 1'b0;
-        DATAIN = 8'bT;
-    #(SYSCLK_PERIOD * 4 )
-        DATAIN = 8'b0;
+        DATAIN = 8'b01010100;
+    
 end
 
 
@@ -60,9 +61,12 @@ end
 //////////////////////////////////////////////////////////////////////
 always @(SYSCLK)
     #(SYSCLK_PERIOD / 2.0) SYSCLK <= !SYSCLK;
-
-//always @(SYSCLK * 
-
+//always @(SYSCLK) begin
+//    #(ORBIT_TIME) DATAIN = 8'b01010100;
+//    #(SYSCLK_PERIOD*20) DATAIN = 8'bz;
+//    #(SYSCLK_PERIOD*20) DATAIN = 8'b01010010;
+//    #(SYSCLK_PERIOD*20) DATAIN = 8'bz;
+//end
 
 //////////////////////////////////////////////////////////////////////
 // Instantiate Unit Under Test:  spi_master
