0eb0021ba73a5aa941a331f687097cc600d1786e
scme2048 <scott.mende@colorado.edu>
2016-02-03 18:45:05 -0700

    Initial flash for spi test. Weird clock stuff but mostly successful!

--- synthesis/synlog.tcl
+++ synthesis/synlog.tcl
@@ -1,1 +1,4 @@
-run_tcl -fg spi_test_syn.tcl
+source "C:/Users/Scott/AppData/Local/Synplicity/scm_perforce.tcl"
+history clear
+project -load spi_test_syn.prj
+project -close C:/Users/Scott/Documents/Docs/School/Senior/Projects/Libero/Ball_Avionics/synthesis/spi_test_syn.prj
